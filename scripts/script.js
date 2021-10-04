ea8af81e52d2cb120b0659c9c791e8c4f01e2aed
Ashutosh Karade <69238714+xR0v3Rx@users.noreply.github.com>
2021-10-04 14:34:18 +0530

    ui done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+
+// declare variables
+const inputForm = document.querySelector("form");
+const userMass = document.getElementById("mass");
+
+//functions
+
+
+//add event listeners
+
+inputForm.addEventListener("submit", submitHandle);
